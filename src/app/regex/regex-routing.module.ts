5fd099b1efe44cc5426324730331b0c19917c69f
Z50 <jantsek.zsolt@gmail.com>
2025-06-05 14:54:11 +0200

    Regex route guard fix

--- src/app/regex/regex-routing.module.ts
+++ src/app/regex/regex-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegexComponent } from './regex.component';
+import { YouCantActivate } from 'src/shared/guards/fourofour.guard';
 
 const routes: Routes = [
-  { path: '', component: RegexComponent }
+  { path: '', component: RegexComponent, canActivate: [YouCantActivate] }
 ];
 
 @NgModule({
